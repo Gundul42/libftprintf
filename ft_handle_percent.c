af4ae7948560619b5ca02bd5b29cc5ad08e7d9be
Gundul <sampah@mailbox.org>
2021-07-03 10:32:13 +0200

    next

--- ft_handle_percent.c
+++ ft_handle_percent.c
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 14:38:47 by graja             #+#    #+#             */
-/*   Updated: 2021/07/02 12:10:45 by graja            ###   ########.fr       */
+/*   Updated: 2021/07/03 10:28:31 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,18 @@
 
 const char	*ft_handle_percent(const char *ptr, t_flags *flags)
 {
-	write(1, "%", 1);
-	if (flags->type != 'R')
-		ptr++;
-	else
-		ptr += flags->i;
-	flags->length++;
+	char	*str;
+
+	str = ft_strjoin("%","");
+	if (flags->zero > 0 && !flags->minus)
+		str = ft_add_zero_front(str, flags->width);
+	if (flags->max)
+		str = ft_add_zero_front(str, flags->max);
+	if (flags->width)
+		str = ft_add_spaces(str, flags);
+	write(1, str, ft_strlen(str));
+	ptr += flags->i;
+	flags->length += ft_strlen(str);
+	free(str);
 	return (ptr);
 }
