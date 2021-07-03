5c2166bc504d949c1dc680a32f08cffea38b33c7
Gundul <sampah@mailbox.org>
2021-07-03 12:52:54 +0200

    jo

--- ft_handle_char.c
+++ ft_handle_char.c
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 17:14:22 by graja             #+#    #+#             */
-/*   Updated: 2021/07/03 12:42:20 by graja            ###   ########.fr       */
+/*   Updated: 2021/07/03 12:50:50 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_handle_char(t_flags *flags)
 		return ;
 	c = va_arg(flags->args, int);
 	str[0] = c;
-	if (c && flags->width)
+	if ((c && flags->width) || (!c && flags->width && !flags->minus))
 		str = ft_add_spaces(str, flags);
 	if (!c && ft_strlen(str))
 		str[ft_strlen(str) - 1] = c;
